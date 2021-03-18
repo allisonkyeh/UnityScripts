9feaaac031767a64936583b7d400b626166ae9fc
allisonkyeh <63689595+allisonkyeh@users.noreply.github.com>
2021-03-18 03:38:24 -0400

    Add files via upload

---
+++ PathOfRaindiance/FollowXZ.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowXZ : MonoBehaviour
+{
+    public Transform PlayerTransform;
+    private Vector3 _objOffset;
+
+    float newPosX = 0;
+    float newPosZ = 0;
+    // og follow code
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _objOffset = transform.position - PlayerTransform.position;
+	}
+
+	// LateUpdate is called after Update
+	void Update ()
+    {
+        // og follow code
+        // Vector3 newPos = PlayerTransform.position + _objOffset;
+        // transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+
+        newPosX = PlayerTransform.position.x + _objOffset.x;
+        newPosZ = PlayerTransform.position.z + _objOffset.z;
+        transform.position = new Vector3(newPosX, 0, newPosZ);
+
+   }
+}
---
+++ PathOfRaindiance/LightningFlash.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningFlash : MonoBehaviour
+{
+    Light dirLight;
+    ParticleSystem source;
+
+    public float flashIntensity;
+    float baseIntensity;
+    int prevFrame;
+    int currFrame;
+
+    public float lerpDuration;
+    float timeElapsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dirLight = GetComponent<Light>();
+        source = GameObject.Find("Lightning Sheet").GetComponent<ParticleSystem>();
+        baseIntensity = dirLight.intensity;
+        prevFrame = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currFrame = source.particleCount;
+        if (currFrame - prevFrame > 0)
+        {
+            StartCoroutine(Flash());
+        }
+        prevFrame = currFrame;
+    }
+
+    IEnumerator Flash()
+    {
+        timeElapsed = 0;
+
+        // delay to match the flash frame of the drawn lightning animation
+        yield return new WaitForSeconds(0.1f);
+
+        while (timeElapsed < lerpDuration)
+        {
+            dirLight.intensity = Mathf.Lerp(flashIntensity, baseIntensity, timeElapsed / lerpDuration);
+            timeElapsed += Time.deltaTime;
+            yield return null; // wait till next frame before continuing
+        }
+        dirLight.intensity = baseIntensity;
+    }
+}
